bb686d488a77e2b024084aaaa62f42adf8cd9d45
r_aru <r_arutkina@mail.ru>
2025-06-30 14:27:12 +0900

    feat: generate cool license plates and implement search using Java collections

--- src/main/java/practice/CoolNumbers.java
+++ src/main/java/practice/CoolNumbers.java
@@ -1,30 +1,56 @@
 package practice;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
 
+    private static final List<Character> LETTERS =
+            Arrays.asList('А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'У', 'Х');
+    private static final int COUNT_OF_NUMBERS = 2000000;
+    private static final Random THIS_IS_CONSTANT = new Random();
+
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+
+        Set<String> coolNumbers = new HashSet<>(COUNT_OF_NUMBERS);
+
+        while (coolNumbers.size() < COUNT_OF_NUMBERS) {
+            String number = generateRandomCoolNumbers();
+            coolNumbers.add(number);
+        }
+        return new ArrayList<>(coolNumbers);
+    }
+    private static String generateRandomCoolNumbers() {
+
+        char carsAmountX = LETTERS.get(THIS_IS_CONSTANT.nextInt(LETTERS.size()));
+        char carsAmountY = LETTERS.get(THIS_IS_CONSTANT.nextInt(LETTERS.size()));
+        char carsAmountZ = LETTERS.get(THIS_IS_CONSTANT.nextInt(LETTERS.size()));
+        int digits = THIS_IS_CONSTANT.nextInt(10) * 100 +
+                THIS_IS_CONSTANT.nextInt(10) * 10 +
+                THIS_IS_CONSTANT.nextInt(10);
+        int region = THIS_IS_CONSTANT.nextInt(199) + 1;
+        return String.format("%c%03d%c%c%02d", carsAmountX, digits, carsAmountY, carsAmountZ, region);
+
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+
+        return list.contains(number);
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+
+        return Collections.binarySearch(sortedList, number) >= 0;
     }
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+
+        return hashSet.contains(number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+
+        return treeSet.contains(number);
     }
 
 }
+
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,14 +1,42 @@
 package practice;
 
-public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
+import java.util.*;
 
+import static practice.CoolNumbers.*;
+
+public class Main {
     public static void main(String[] args) {
+
+        List<String> coolNumbers = generateCoolNumbers();
+        HashSet<String> hashSet = new HashSet<>(coolNumbers);
+        TreeSet<String> treeSet = new TreeSet<>(coolNumbers);
+        String numberToFind = coolNumbers.get(new Random().nextInt(coolNumbers.size()));
+
+        long startTimeOverdoingTheSymbol = System.nanoTime();
+        boolean foundOverdoingTheSymbol = bruteForceSearchInList(coolNumbers, numberToFind);
+        long durationOverdoingTheSymbol = System.nanoTime() - startTimeOverdoingTheSymbol;
+        System.out.println("Поиск перебором: номер " + (foundOverdoingTheSymbol ? "найден" : "не найден") + ", " +
+                "поиск занял " + durationOverdoingTheSymbol + "нс");
+
+        Collections.sort(coolNumbers);
+        long startTimeBinary = System.nanoTime();
+        boolean foundBinary = binarySearchInList(coolNumbers, numberToFind);
+        long durationBinary = System.nanoTime() - startTimeBinary;
+        System.out.println("Бинарный поиск: номер " + (foundBinary ? "найден" : "не найден") + ", " +
+                "поиск занял " + durationBinary + "нс");
+
+        long startTimeHashSet = System.nanoTime();
+        boolean foundHashSet = searchInHashSet(hashSet, numberToFind);
+        long durationHashSet = System.nanoTime() - startTimeHashSet;
+        System.out.println("Поиск в HashSet: номер " + (foundHashSet ? "найден" : "не найден") + ", " +
+                "поиск занял " + durationHashSet + "нс");
+
+        long startTimeTreeSet = System.nanoTime();
+        boolean foundTreeSet = searchInTreeSet(treeSet, numberToFind);
+        long durationTreeSet = System.nanoTime() - startTimeTreeSet;
+        System.out.println("Поиск в TreeSet: номер " + (foundTreeSet ? "найден" : "не найден") + ", " +
+                "поиск занял " + durationTreeSet + "нс");
 
     }
 }
+
